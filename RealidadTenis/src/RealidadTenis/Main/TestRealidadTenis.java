79fce21c6484fb0f6cdc0746c0c6e7ee73aad3ad
Adriel Gomez <gomez.adriel.i@gmail.com>
2021-10-22 20:02:30 -0300

    cambios db

--- RealidadTenis/src/RealidadTenis/Main/TestRealidadTenis.java
+++ RealidadTenis/src/RealidadTenis/Main/TestRealidadTenis.java
@@ -1,13 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package RealidadTenis.Main;
 
-/**
- *
- * @author Agostina
- */
+
 public class TestRealidadTenis {
+
+    public static void main(String[] args) {
+        
+    }
     
 }
